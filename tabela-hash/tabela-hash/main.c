c1258654c7396f7b4018191667468b24fd88087a
Leonardo Rossi <08.leo.rossi@gmail.com>
2023-02-21 07:44:39 -0300

    First Commit

---
+++ tabela-hash/tabela-hash/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  tabela-hash
+//
+//  Created by Leonardo Rossi on 19/02/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
